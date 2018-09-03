e2c88e278123da39dc36cd7a16fa63fcea84310b
Сердюк Никита <n.serdyuk@orglot.office>
2018-09-03 16:09:07 +0300

    Added github pages.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment: IEnvironment = {
   production: false,
   squareWidth: 10,
   squareHeight: 10,
-  testMode: true,
+  testMode: false,
   ships: [
     {
       shipForm: 'L',
